87d2733cc60892d96609fe09e152a9f724194596
7up <23521320@gm.uit.edu.vn>
2024-08-03 11:49:40 +0700

    Added a running rhino animator

---
+++ Assets/Rhino_run.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rhino_run : StateMachineBehaviour
+{
+    Rigidbody2D rhino;
+    Transform player;
+    public float speed = 3f;
+    public float OutRange = 8f;
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rhino = animator.GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector2 target = new Vector2(player.position.x, rhino.position.y);
+        Vector2 moveDirection = Vector2.MoveTowards(rhino.position, target, speed * Time.fixedDeltaTime);
+        rhino.MovePosition(moveDirection);
+        if(Vector2.Distance(rhino.position, player.position) > OutRange)
+        {
+            animator.SetTrigger("Idle"); 
+        }
+    }
+}
